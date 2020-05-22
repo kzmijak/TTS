ed524bbeb8ce979d036903144d13e25d1ba9a8de
kzmijak <kzmijak@gmail.com>
2020-05-22 12:56:35 +0200

    This is the final version of the code with tests

---
+++ TTS.Tests/ExerciseTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TTS;
+using Xunit;
+
+namespace TTS.Tests
+{
+    public class ExerciseTests
+    {
+        [Theory]
+        [InlineData(3,0.66, 1.5, 3)]
+        [InlineData(3,1, 1.5, -1)]
+        public void StartExercise_ShouldValidateData(double h, double b, double w, double expected)
+        {
+            // Arrange
+            // Handled by XUnit
+
+            // Act
+            double actual = TTS.Exercise.StartExercise(h, b, w);
+
+            // Assert
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
---
+++ TTS/Exercise.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TTS
+{
+    public class Exercise
+    {
+        public static double StartExercise(double h, double b, double w)
+        {
+            if ( h < 0  || w <0 || b < 0 || b >= 1) // I assume that if (at least) one of these requirements are fulfilled, the parameters are not valid
+                                          // - starting and window height is lower than 0 ( I assume that calculations are made on or above sea level (0 meters),
+                                          //   height cannot be lower than 0, but can equal to 0, and can be infinite
+                                          // - bouncing modifier ( b ) can only have values between 0 and lower than 1. ( 0 because it may not bounce whatsoever)
+            {
+                return -1;
+            }
+
+
+            short counter = 0; /// this is the counter of bounces visible by the window (w)
+            while( h >= w)
+            {
+                counter += 1; // This is when mother sees the ball descend
+                h *= b; // height is now modified by the bouncing modifier
+
+                if(h >= w )
+                {
+                    counter += 1; // This is when mother sees the ball ascend
+                }
+            }
+
+            return counter;
+        }
+    }
+}
